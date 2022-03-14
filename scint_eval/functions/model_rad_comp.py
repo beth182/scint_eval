c701892886b27b724a6f84440283ce67ac5e49d4
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-03-14 15:22:35 +0000

    plot with just incoming long and short rad

--- scint_eval/functions/model_rad_comp.py
+++ scint_eval/functions/model_rad_comp.py
@@ -17,8 +17,8 @@ from scint_eval.functions import roughness
 ########################################################################################################################
 # choices
 
-DOYstart = 2016126
-DOYstop = 2016126
+DOYstart = 2016123
+DOYstop = 2016123
 model_format = 'new'
 scint_path = 12
 obs_site = 'IMU'
@@ -72,21 +72,21 @@ files_obs = file_read.finding_files(model_format, 'obs', DOYstart, DOYstop, 'KSS
 z0zdlist = roughness.roughness_and_displacement(1, 0.8, look_up.obs_z0_macdonald[obs_site],
                                                 look_up.obs_zd_macdonald[obs_site])
 
-obs_lup = observations.sort_obs('lup', files_obs, DOYstart, DOYstop, obs_site, z0zdlist, saveyn,
-                                savepath, sample,
-                                instrument)
+# obs_lup = observations.sort_obs('lup', files_obs, DOYstart, DOYstop, obs_site, z0zdlist, saveyn,
+#                                 savepath, sample,
+#                                 instrument)
+#
+# group_obs_lup = [obs_lup[4], obs_lup[2], obs_lup[3], obs_lup[0], obs_lup[1], obs_lup[5]]
+# obs_time_lup, obs_vals_lup = array_retrieval.retrive_arrays_obs(group_obs_lup)
+# obs_time_lup, obs_vals_lup = array_retrieval.rm_nans(obs_time_lup, obs_vals_lup)
 
-group_obs_lup = [obs_lup[4], obs_lup[2], obs_lup[3], obs_lup[0], obs_lup[1], obs_lup[5]]
-obs_time_lup, obs_vals_lup = array_retrieval.retrive_arrays_obs(group_obs_lup)
-obs_time_lup, obs_vals_lup = array_retrieval.rm_nans(obs_time_lup, obs_vals_lup)
-
-obs_kup = observations.sort_obs('kup', files_obs, DOYstart, DOYstop, obs_site, z0zdlist, saveyn,
-                                savepath, sample,
-                                instrument)
-
-group_obs_kup = [obs_kup[6], obs_kup[3], obs_kup[4], obs_kup[0], obs_kup[1], obs_kup[8]]
-obs_time_kup, obs_vals_kup = array_retrieval.retrive_arrays_obs(group_obs_kup)
-obs_time_kup, obs_vals_kup = array_retrieval.rm_nans(obs_time_kup, obs_vals_kup)
+# obs_kup = observations.sort_obs('kup', files_obs, DOYstart, DOYstop, obs_site, z0zdlist, saveyn,
+#                                 savepath, sample,
+#                                 instrument)
+#
+# group_obs_kup = [obs_kup[6], obs_kup[3], obs_kup[4], obs_kup[0], obs_kup[1], obs_kup[8]]
+# obs_time_kup, obs_vals_kup = array_retrieval.retrive_arrays_obs(group_obs_kup)
+# obs_time_kup, obs_vals_kup = array_retrieval.rm_nans(obs_time_kup, obs_vals_kup)
 
 obs_kdown = observations.sort_obs('kdown', files_obs, DOYstart, DOYstop, obs_site, z0zdlist, saveyn,
                                   savepath, sample,
@@ -104,85 +104,85 @@ group_obs_ldown = [obs_ldown[4], obs_ldown[2], obs_ldown[3], obs_ldown[0], obs_l
 obs_time_ldown, obs_vals_ldown = array_retrieval.retrive_arrays_obs(group_obs_ldown)
 obs_time_ldown, obs_vals_ldown = array_retrieval.rm_nans(obs_time_ldown, obs_vals_ldown)
 
-assert obs_time_kdown.all() == obs_time_kup.all()
-kstar = obs_vals_kdown - obs_vals_kup
-
-assert obs_time_ldown.all() == obs_time_lup.all()
-lstar = obs_vals_ldown - obs_vals_lup
-
-assert obs_time_ldown.all() == obs_time_kdown.all()
-qstar = lstar + kstar
-
-print('end')
-
-########################################################################################################################
-# lstar
-# file_read.py
-file_dict_ukv_lstar = file_read.finding_files(model_format,
-                                              'ukv',
-                                              DOYstart_mod,
-                                              DOYstop_mod,
-                                              'IMU',
-                                              run,
-                                              instrument,
-                                              sample,
-                                              'lstar',
-                                              obs_level,
-                                              model_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_storage/"
-                                              )
-
-# ordering UKV model files
-# file_read.py
-files_ukv_lstar = file_read.order_model_stashes('ukv', file_dict_ukv_lstar, 'lstar')
-
-ukv_lstar = sort_model.sort_models('lstar', 'ukv', files_ukv_lstar, 0, DOYstart, DOYstop,
-                                   'IMU', savepath, model_format, grid_choice='E')
-
-# define dict for included models
-included_models_lstar = {}
-
-# stringtimelon, stringtemplon, lontimedict, lontempdict, modheightvaluelon
-group_ukv_lstar = [ukv_lstar[5], ukv_lstar[6], ukv_lstar[0], ukv_lstar[1], ukv_lstar[10]]
-# append to dict
-included_models_lstar['ukv'] = group_ukv_lstar
-
-mod_time_lstar, mod_vals_lstar = array_retrieval.retrive_arrays_model(included_models_lstar, 'ukv')
+# assert obs_time_kdown.all() == obs_time_kup.all()
+# kstar = obs_vals_kdown - obs_vals_kup
+#
+# assert obs_time_ldown.all() == obs_time_lup.all()
+# lstar = obs_vals_ldown - obs_vals_lup
+#
+# assert obs_time_ldown.all() == obs_time_kdown.all()
+# qstar = lstar + kstar
 
 print('end')
 
-# lstar
-# file_read.py
-file_dict_ukv_lstar_KSSW = file_read.finding_files(model_format,
-                                                   'ukv',
-                                                   DOYstart_mod,
-                                                   DOYstop_mod,
-                                                   'KSSW',
-                                                   run,
-                                                   instrument,
-                                                   sample,
-                                                   'lstar',
-                                                   obs_level,
-                                                   model_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_storage/"
-                                                   )
-
-# ordering UKV model files
-# file_read.py
-files_ukv_lstar_KSSW = file_read.order_model_stashes('ukv', file_dict_ukv_lstar_KSSW, 'lstar')
-
-ukv_lstar_KSSW = sort_model.sort_models('lstar', 'ukv', files_ukv_lstar_KSSW, 0, DOYstart, DOYstop,
-                                        'KSSW', savepath, model_format, grid_choice='E')
-
-# define dict for included models
-included_models_lstar_KSSW = {}
-
-# stringtimelon, stringtemplon, lontimedict, lontempdict, modheightvaluelon
-group_ukv_lstar_KSSW = [ukv_lstar_KSSW[5], ukv_lstar_KSSW[6], ukv_lstar_KSSW[0], ukv_lstar_KSSW[1], ukv_lstar_KSSW[10]]
-# append to dict
-included_models_lstar_KSSW['ukv'] = group_ukv_lstar_KSSW
-
-mod_time_lstar_KSSW, mod_vals_lstar_KSSW = array_retrieval.retrive_arrays_model(included_models_lstar_KSSW, 'ukv')
-
-print('end')
+# ########################################################################################################################
+# # lstar
+# # file_read.py
+# file_dict_ukv_lstar = file_read.finding_files(model_format,
+#                                               'ukv',
+#                                               DOYstart_mod,
+#                                               DOYstop_mod,
+#                                               'IMU',
+#                                               run,
+#                                               instrument,
+#                                               sample,
+#                                               'lstar',
+#                                               obs_level,
+#                                               model_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_storage/"
+#                                               )
+#
+# # ordering UKV model files
+# # file_read.py
+# files_ukv_lstar = file_read.order_model_stashes('ukv', file_dict_ukv_lstar, 'lstar')
+#
+# ukv_lstar = sort_model.sort_models('lstar', 'ukv', files_ukv_lstar, 0, DOYstart, DOYstop,
+#                                    'IMU', savepath, model_format, grid_choice='E')
+#
+# # define dict for included models
+# included_models_lstar = {}
+#
+# # stringtimelon, stringtemplon, lontimedict, lontempdict, modheightvaluelon
+# group_ukv_lstar = [ukv_lstar[5], ukv_lstar[6], ukv_lstar[0], ukv_lstar[1], ukv_lstar[10]]
+# # append to dict
+# included_models_lstar['ukv'] = group_ukv_lstar
+#
+# mod_time_lstar, mod_vals_lstar = array_retrieval.retrive_arrays_model(included_models_lstar, 'ukv')
+#
+# print('end')
+#
+# # lstar
+# # file_read.py
+# file_dict_ukv_lstar_KSSW = file_read.finding_files(model_format,
+#                                                    'ukv',
+#                                                    DOYstart_mod,
+#                                                    DOYstop_mod,
+#                                                    'KSSW',
+#                                                    run,
+#                                                    instrument,
+#                                                    sample,
+#                                                    'lstar',
+#                                                    obs_level,
+#                                                    model_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_storage/"
+#                                                    )
+#
+# # ordering UKV model files
+# # file_read.py
+# files_ukv_lstar_KSSW = file_read.order_model_stashes('ukv', file_dict_ukv_lstar_KSSW, 'lstar')
+#
+# ukv_lstar_KSSW = sort_model.sort_models('lstar', 'ukv', files_ukv_lstar_KSSW, 0, DOYstart, DOYstop,
+#                                         'KSSW', savepath, model_format, grid_choice='E')
+#
+# # define dict for included models
+# included_models_lstar_KSSW = {}
+#
+# # stringtimelon, stringtemplon, lontimedict, lontempdict, modheightvaluelon
+# group_ukv_lstar_KSSW = [ukv_lstar_KSSW[5], ukv_lstar_KSSW[6], ukv_lstar_KSSW[0], ukv_lstar_KSSW[1], ukv_lstar_KSSW[10]]
+# # append to dict
+# included_models_lstar_KSSW['ukv'] = group_ukv_lstar_KSSW
+#
+# mod_time_lstar_KSSW, mod_vals_lstar_KSSW = array_retrieval.retrive_arrays_model(included_models_lstar_KSSW, 'ukv')
+#
+# print('end')
 
 ########################################################################################################################
 # ldown
@@ -322,30 +322,30 @@ mod_time_kdown_KSSW, mod_vals_kdown_KSSW = array_retrieval.retrive_arrays_model(
 
 ########################################################################################################################
 
-assert mod_time_kdown.all() == mod_time_ldown.all() == mod_time_lstar.all()
-assert mod_time_kdown_KSSW.all() == mod_time_ldown_KSSW.all() == mod_time_lstar_KSSW.all()
-assert mod_time_kdown_KSSW.all() == mod_time_kdown.all()
-
-lup_ukv = mod_vals_lstar - mod_vals_ldown
-lup_ukv_KSSW = mod_vals_lstar_KSSW - mod_vals_ldown_KSSW
+# assert mod_time_kdown.all() == mod_time_ldown.all() == mod_time_lstar.all()
+# assert mod_time_kdown_KSSW.all() == mod_time_ldown_KSSW.all() == mod_time_lstar_KSSW.all()
+# assert mod_time_kdown_KSSW.all() == mod_time_kdown.all()
+#
+# lup_ukv = mod_vals_lstar - mod_vals_ldown
+# lup_ukv_KSSW = mod_vals_lstar_KSSW - mod_vals_ldown_KSSW
 
 plt.close('all')
 plt.figure()
 plt.scatter(obs_time_kdown, obs_vals_kdown, label='$K_{\downarrow}$', color='red', marker='x')
 plt.scatter(obs_time_ldown, obs_vals_ldown, label='$L_{\downarrow}$', color='blue', marker='x')
-plt.scatter(obs_time_kup, obs_vals_kup, label=r'$K_{\uparrow}$', color='green', marker='x')
-plt.scatter(obs_time_lup, -obs_vals_lup, label=r'$L_{\uparrow}$', color='gold', marker='x')
-plt.scatter(obs_time_kdown, kstar, label='K*', color='grey', marker='x')
-plt.scatter(obs_time_ldown, lstar, label='L*', color='paleturquoise', marker='x')
-plt.scatter(obs_time_ldown, qstar, label='Q*', color='black', marker='x')
+# plt.scatter(obs_time_kup, obs_vals_kup, label=r'$K_{\uparrow}$', color='green', marker='x')
+# plt.scatter(obs_time_lup, -obs_vals_lup, label=r'$L_{\uparrow}$', color='gold', marker='x')
+# plt.scatter(obs_time_kdown, kstar, label='K*', color='grey', marker='x')
+# plt.scatter(obs_time_ldown, lstar, label='L*', color='paleturquoise', marker='x')
+# plt.scatter(obs_time_ldown, qstar, label='Q*', color='black', marker='x')
 
 plt.plot(mod_time_kdown + dt.timedelta(minutes=15), mod_vals_ldown, color='blue', label='UKV IMU')
 plt.plot(mod_time_kdown + dt.timedelta(minutes=15), mod_vals_ldown_KSSW, color='blue', linestyle='dotted',
          label='UKV KSSW')
-plt.plot(mod_time_kdown + dt.timedelta(minutes=15), mod_vals_lstar, color='paleturquoise')
-plt.plot(mod_time_kdown + dt.timedelta(minutes=15), mod_vals_lstar_KSSW, color='paleturquoise', linestyle='dotted')
-plt.plot(mod_time_kdown + dt.timedelta(minutes=15), lup_ukv, color='gold')
-plt.plot(mod_time_kdown + dt.timedelta(minutes=15), lup_ukv_KSSW, color='gold', linestyle='dotted')
+# plt.plot(mod_time_kdown + dt.timedelta(minutes=15), mod_vals_lstar, color='paleturquoise')
+# plt.plot(mod_time_kdown + dt.timedelta(minutes=15), mod_vals_lstar_KSSW, color='paleturquoise', linestyle='dotted')
+# plt.plot(mod_time_kdown + dt.timedelta(minutes=15), lup_ukv, color='gold')
+# plt.plot(mod_time_kdown + dt.timedelta(minutes=15), lup_ukv_KSSW, color='gold', linestyle='dotted')
 plt.plot(mod_time_kdown + dt.timedelta(minutes=15), mod_vals_kdown, color='red')
 plt.plot(mod_time_kdown + dt.timedelta(minutes=15), mod_vals_kdown_KSSW, color='red', linestyle='dotted')
 

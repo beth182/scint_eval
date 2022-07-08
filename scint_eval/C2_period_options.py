e950a07c5fe39d7ae47a87769e132291e8412163
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-07-08 11:22:07 +0100

    updates to data selection

--- scint_eval/C2_period_options.py
+++ scint_eval/C2_period_options.py
@@ -65,35 +65,31 @@ else:
 
 DOYstop_mod = DOY_stop - 1
 
-
-
-
 files_obs15 = file_read.finding_files('new', 'obs', DOY_start, DOY_stop, 'SWT', '21Z', 'LASMkII_Fast', '15min',
-                                    'H', 'L1',
-                                    obs_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_raw/data/"
-                                    )
+                                      'H', 'L1',
+                                      obs_path='C:/Users/beths/Desktop/LANDING/data_wifi_problems/data/'
+                                      # obs_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_raw/data/"
+                                      )
 
 z0zdlist15 = roughness.roughness_and_displacement(1, 0.8, look_up.obs_z0_macdonald['SWT'],
-                                                look_up.obs_zd_macdonald['SWT'])
-
-
+                                                  look_up.obs_zd_macdonald['SWT'])
 
 files_obs12 = file_read.finding_files('new', 'obs', DOY_start, DOY_stop, 'IMU', '21Z', 'LASMkII_Fast', '15min',
-                                    'H', 'L1',
-                                    obs_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_raw/data/"
-                                    )
+                                      'H', 'L1',
+                                      obs_path='C:/Users/beths/Desktop/LANDING/data_wifi_problems/data/'
+                                      # obs_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_raw/data/"
+                                      )
 
 z0zdlist12 = roughness.roughness_and_displacement(1, 0.8, look_up.obs_z0_macdonald['IMU'],
-                                                look_up.obs_zd_macdonald['IMU'])
+                                                  look_up.obs_zd_macdonald['IMU'])
 
 obs12 = observations.sort_obs('H', files_obs12, DOY_start, DOY_stop, 'IMU', z0zdlist12, 1,
-                                                'C:/Users/beths/Desktop/LANDING/', '15min',
-                                                'LASMkII_Fast')
-
+                              'C:/Users/beths/Desktop/LANDING/', '15min',
+                              'LASMkII_Fast')
 
 obs15 = observations.sort_obs('H', files_obs15, DOY_start, DOY_stop, 'SWT', z0zdlist15, 1,
-                                                'C:/Users/beths/Desktop/LANDING/', '15min',
-                                                'LASMkII_Fast')
+                              'C:/Users/beths/Desktop/LANDING/', '15min',
+                              'LASMkII_Fast')
 
 group_obs12 = [obs12[4], obs12[2], obs12[3], obs12[0], obs12[1], obs12[5]]
 group_obs15 = [obs15[4], obs15[2], obs15[3], obs15[0], obs15[1], obs15[5]]
@@ -102,11 +98,8 @@ obs_time12, obs_vals12 = array_retrieval.retrive_arrays_obs(group_obs12)
 
 obs_time15, obs_vals15 = array_retrieval.retrive_arrays_obs(group_obs15)
 
-
-
 disheight12 = obs12[6]
 disheight15 = obs15[6]
-
 
 print('END')
 
@@ -121,9 +114,9 @@ file_dict_ukv_12 = file_read.finding_files('new',
                                            '15min',
                                            'BL_H',
                                            'L1',
-                                           model_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_storage/"
+                                           model_path='C:/Users/beths/Desktop/LANDING/data_wifi_problems/data/'
+                                           # model_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_storage/"
                                            )
-
 
 file_dict_ukv_15 = file_read.finding_files('new',
                                            'ukv',
@@ -135,7 +128,8 @@ file_dict_ukv_15 = file_read.finding_files('new',
                                            '15min',
                                            'BL_H',
                                            'L1',
-                                           model_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_storage/"
+                                           model_path='C:/Users/beths/Desktop/LANDING/data_wifi_problems/data/'
+                                           # model_path="//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_storage/"
                                            )
 
 files_ukv_12 = file_read.order_model_stashes('ukv', file_dict_ukv_12, 'BL_H')
@@ -156,30 +150,27 @@ included_BL_H15 = {'BL_H_15': BL_H_15_list}
 mod_time_12, mod_vals_12 = array_retrieval.retrive_arrays_model(included_BL_H12, 'BL_H_12')
 mod_time_15, mod_vals_15 = array_retrieval.retrive_arrays_model(included_BL_H15, 'BL_H_15')
 
-
-
-
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 
-plt.figure(figsize=(15,7))
+plt.figure(figsize=(15, 4))
 ax = plt.subplot(1, 1, 1)
 
-plt.scatter(obs_time12, obs_vals12, label='BCT_IMU', marker='.')
-plt.scatter(obs_time15, obs_vals15, label='SCT_SWT', marker='.')
+plt.scatter(obs_time12, obs_vals12, label='BCT_IMU', marker='.', color='blue')
+plt.scatter(obs_time15, obs_vals15, label='SCT_SWT', marker='.', color='red')
 
-plt.plot(mod_time_12,mod_vals_12, label='UKV IMU')
-plt.plot(mod_time_15,mod_vals_15, label='UKV SWT')
-
-
+plt.plot(mod_time_12, mod_vals_12, label='UKV IMU', color='blue')
+plt.plot(mod_time_15, mod_vals_15, label='UKV SWT', color='red')
 
 ax.xaxis.set_major_formatter(DateFormatter('%j'))
 
-plt.legend()
-plt.ylabel('Sensible Heat Flux (W m$^{-2}$)')
+# plt.legend()
+plt.ylabel('$Q_{H}$ (W m$^{-2}$)')
 plt.xlabel('DOY')
 
-plt.show()
+import numpy as np
 
+plt.savefig('C:/Users/beths/Desktop/LANDING/save.png', bbox_inches='tight')
+# plt.show()
 
 print('END')
--- scint_eval/functions/transect_plots.py
+++ scint_eval/functions/transect_plots.py
@@ -104,33 +104,37 @@ def plot_transects(path_11, pt_11,
     # #                        label='Building heights')
     # dem_bar_11 = ax1.bar(pt_11.gdf.index * pt_11.point_res, pt_11.gdf["z_asl_max_dem"], hori_res, color='brown')
 
-    bld_bar_11 = ax1.bar(pt_11.gdf.index * pt_11.point_res, pt_11.gdf["z_asl_max_bdsm"] - pt_11.gdf["z_asl_max_dem"], hori_res, color='grey')
-    dem_bar_11 = ax1.bar(pt_11.gdf.index * pt_11.point_res, pt_11.gdf["z_asl_max_dem"]*-1, hori_res, color='sienna')
-
+    bld_bar_11 = ax1.bar(pt_11.gdf.index * pt_11.point_res, pt_11.gdf["z_asl_max_bdsm"] - pt_11.gdf["z_asl_max_dem"],
+                         hori_res, color='grey')
+    dem_bar_11 = ax1.bar(pt_11.gdf.index * pt_11.point_res, pt_11.gdf["z_asl_max_dem"] * -1, hori_res, color='sienna')
 
     # bld_bar_12 = ax2.bar(pt_12.gdf.index * pt_12.point_res, pt_12.gdf["z_asl_max_bdsm"], hori_res, color='grey')
     # # bld_line_12 = ax2.plot(pt_12.gdf.index * pt_12.point_res, pt_12.gdf["z_asl_max"], color='blue',
     # #                        label='Building heights')
-    bld_bar_12 = ax2.bar(pt_12.gdf.index * pt_12.point_res, pt_12.gdf["z_asl_max_bdsm"] - pt_12.gdf["z_asl_max_dem"], hori_res, color='grey')
-    dem_bar_12 = ax2.bar(pt_12.gdf.index * pt_12.point_res, pt_12.gdf["z_asl_max_dem"]*-1, hori_res, color='sienna')
+    bld_bar_12 = ax2.bar(pt_12.gdf.index * pt_12.point_res, pt_12.gdf["z_asl_max_bdsm"] - pt_12.gdf["z_asl_max_dem"],
+                         hori_res, color='grey')
+    dem_bar_12 = ax2.bar(pt_12.gdf.index * pt_12.point_res, pt_12.gdf["z_asl_max_dem"] * -1, hori_res, color='sienna')
 
     # bld_bar_13 = ax3.bar(pt_13.gdf.index * pt_13.point_res, pt_13.gdf["z_asl_max_bdsm"], hori_res, color='grey')
     # # bld_line_13 = ax3.plot(pt_13.gdf.index * pt_13.point_res, pt_13.gdf["z_asl_max"], color='blue',
     # #                        label='Building heights')
-    bld_bar_13 = ax3.bar(pt_13.gdf.index * pt_13.point_res, pt_13.gdf["z_asl_max_bdsm"] - pt_13.gdf["z_asl_max_dem"], hori_res, color='grey')
-    dem_bar_13 = ax3.bar(pt_13.gdf.index * pt_13.point_res, pt_13.gdf["z_asl_max_dem"]*-1, hori_res, color='sienna')
+    bld_bar_13 = ax3.bar(pt_13.gdf.index * pt_13.point_res, pt_13.gdf["z_asl_max_bdsm"] - pt_13.gdf["z_asl_max_dem"],
+                         hori_res, color='grey')
+    dem_bar_13 = ax3.bar(pt_13.gdf.index * pt_13.point_res, pt_13.gdf["z_asl_max_dem"] * -1, hori_res, color='sienna')
 
     # bld_bar_14 = ax4.bar(pt_14.gdf.index * pt_14.point_res, pt_14.gdf["z_asl_max_bdsm"], hori_res, color='grey')
     # # bld_line_14 = ax4.plot(pt_14.gdf.index * pt_14.point_res, pt_14.gdf["z_asl_max"], color='blue',
     # #                        label='Building heights')
-    bld_bar_14 = ax4.bar(pt_14.gdf.index * pt_14.point_res, pt_14.gdf["z_asl_max_bdsm"] - pt_14.gdf["z_asl_max_dem"], hori_res, color='grey')
-    dem_bar_14 = ax4.bar(pt_14.gdf.index * pt_14.point_res, pt_14.gdf["z_asl_max_dem"]*-1, hori_res, color='sienna')
+    bld_bar_14 = ax4.bar(pt_14.gdf.index * pt_14.point_res, pt_14.gdf["z_asl_max_bdsm"] - pt_14.gdf["z_asl_max_dem"],
+                         hori_res, color='grey')
+    dem_bar_14 = ax4.bar(pt_14.gdf.index * pt_14.point_res, pt_14.gdf["z_asl_max_dem"] * -1, hori_res, color='sienna')
 
     # bld_bar_15 = ax5.bar(pt_15.gdf.index * pt_15.point_res, pt_15.gdf["z_asl_max_bdsm"], hori_res, color='grey')
     # # bld_line_15 = ax5.plot(pt_15.gdf.index * pt_15.point_res, pt_15.gdf["z_asl_max"], color='blue',
     # #                        label='Building heights')
-    bld_bar_15 = ax5.bar(pt_15.gdf.index * pt_15.point_res, pt_15.gdf["z_asl_max_bdsm"] - pt_15.gdf["z_asl_max_dem"], hori_res, color='grey')
-    dem_bar_15 = ax5.bar(pt_15.gdf.index * pt_15.point_res, pt_15.gdf["z_asl_max_dem"]*-1, hori_res, color='sienna')
+    bld_bar_15 = ax5.bar(pt_15.gdf.index * pt_15.point_res, pt_15.gdf["z_asl_max_bdsm"] - pt_15.gdf["z_asl_max_dem"],
+                         hori_res, color='grey')
+    dem_bar_15 = ax5.bar(pt_15.gdf.index * pt_15.point_res, pt_15.gdf["z_asl_max_dem"] * -1, hori_res, color='sienna')
 
     # plot the path
     path_line_11 = ax1.plot(pt_11.gdf.path_length_m, pt_11.gdf["path_height_asl"], color='blue',
@@ -215,17 +219,119 @@ def plot_transects(path_11, pt_11,
     print('end')
 
 
+def plot_2_transects(path_12, pt_12,
+                     path_15, pt_15,
+                     hori_res,
+                     pw_fun=sct.path_weight.bessel_approx):
+    # which path is the longest?
+    path_list = [path_12, path_15]
+    path_length_list = [i.path_length() for i in path_list]
+    x_lim_max = max(path_length_list)
+
+    # which path has the highest element
+    transect_list = [pt_12, pt_15]
+    transect_max_list = [max(i.gdf["z_asl_max_bdsm"]) for i in transect_list]
+    y_lim_max = max(transect_max_list)
+
+    fig = plt.figure(constrained_layout=True, figsize=(10, 6))
+    spec = gridspec.GridSpec(ncols=1, nrows=2)
+
+    ax1 = fig.add_subplot(spec[0])
+    ax2 = fig.add_subplot(spec[1])
+
+    # plot the path weighting curve
+    path_weight_df_12 = path_weight.path_weight(fx=pw_fun, n_x=pt_12.gdf.shape[0])
+    path_weight_df_15 = path_weight.path_weight(fx=pw_fun, n_x=pt_15.gdf.shape[0])
+
+    ax1_t = ax1.twinx()
+    ax2_t = ax2.twinx()
+
+    pw_line_12 = ax1_t.plot(pt_12.gdf.index * pt_12.point_res,
+                            path_weight_df_12["path_weight"], color="magenta", linestyle='--',
+                            label='Path weighting function', alpha=0.5)
+    pw_line_15 = ax2_t.plot(pt_15.gdf.index * pt_15.point_res,
+                            path_weight_df_15["path_weight"], color="magenta", linestyle='--',
+                            label='Path weighting function', alpha=0.5)
+
+    ax1.spines['right'].set_color('magenta')
+    ax1_t.spines['right'].set_color('magenta')
+    ax1_t.yaxis.label.set_color('magenta')
+    ax1_t.tick_params(axis='y', colors='magenta')
+
+    ax2.spines['right'].set_color('magenta')
+    ax2_t.spines['right'].set_color('magenta')
+    ax2_t.yaxis.label.set_color('magenta')
+    ax2_t.tick_params(axis='y', colors='magenta')
+
+    plt.setp(ax2_t.get_yticklabels()[-1], visible=False)
+
+    bld_bar_12 = ax1.bar(pt_12.gdf.index * pt_12.point_res, pt_12.gdf["z_asl_max_bdsm"],
+                         hori_res, color='grey')
+    dem_bar_12 = ax1.bar(pt_12.gdf.index * pt_12.point_res, pt_12.gdf["z_asl_max_dem"], hori_res, color='sienna')
+
+    bld_bar_15 = ax2.bar(pt_15.gdf.index * pt_15.point_res, pt_15.gdf["z_asl_max_bdsm"],
+                         hori_res, color='grey')
+    dem_bar_15 = ax2.bar(pt_15.gdf.index * pt_15.point_res, pt_15.gdf["z_asl_max_dem"], hori_res, color='sienna')
+
+    # plot the path
+    path_line_12 = ax1.plot(pt_12.gdf.path_length_m, pt_12.gdf["path_height_asl"], color='blue',
+                            label='LAS path')
+
+    path_line_15 = ax2.plot(pt_15.gdf.path_length_m, pt_15.gdf["path_height_asl"], color='red',
+                            label='LAS path')
+
+    ax1.set_xticks([])
+
+    ax1.set_xlim(0, x_lim_max)
+    ax2.set_xlim(0, x_lim_max)
+
+
+    ax1.set_ylim(0, y_lim_max+5)
+    ax2.set_ylim(0, y_lim_max+5)
+
+    # add effective beam height label
+    ebh_12 = pt_12.effective_beam_height()
+    ebh_15 = pt_15.effective_beam_height()
+
+    plt.text(0.94, 0.94,
+             str(path_12.pair_id.split('_')[0] + r' $\rightarrow$ ' + path_12.pair_id.split('_')[1]),
+             horizontalalignment='center', transform=ax1.transAxes, color='blue')
+
+    plt.text(0.94, 0.94,
+             str(path_15.pair_id.split('_')[0] + r' $\rightarrow$ ' + path_15.pair_id.split('_')[1]),
+             horizontalalignment='center', transform=ax2.transAxes, color='red')
+
+    plt.text(0.5, 0.6, '$z_{fb}$ = %d.2 m agl' % ebh_12, horizontalalignment='center', transform=ax1.transAxes,
+             color='green')
+    plt.text(0.5, 0.2, '$z_{fb}$ = %d.2 m agl' % ebh_15, horizontalalignment='center', transform=ax2.transAxes,
+             color='green')
+
+    ax1.plot(pt_12.gdf.path_length_m, ebh_12 + pt_12.gdf["z_asl_max_dem"], color='green', linestyle=':')
+    ax2.plot(pt_15.gdf.path_length_m, ebh_15 + pt_15.gdf["z_asl_max_dem"], color='green', linestyle=':')
+
+    ax2.set_xlabel('Horizontal distance (m)')
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+
+    fig.text(0.075, 0.5, 'Height asl (m)', va='center', rotation='vertical')
+    fig.text(0.94, 0.5, 'Path Weighting', va='center', rotation='vertical', color='magenta')
+
+    plt.savefig('C:/Users/beths/OneDrive - University of Reading/Paper 2/Plan/path_transect.png', bbox_inches='tight')
+
+    print('end')
+
+
 ########################################################################################################################
 hori_res = 10
 
 # path definition
 
-# path 11 - BTT -> BCT
-path_11 = sct.ScintillometerPair(x=[282251.14, 285440.6056],
-                                 y=[5712486.47, 5712253.017],
-                                 z_asl=[180, 142],
-                                 pair_id='BTT_BCT',
-                                 crs='epsg:32631')
+# # path 11 - BTT -> BCT
+# path_11 = sct.ScintillometerPair(x=[282251.14, 285440.6056],
+#                                  y=[5712486.47, 5712253.017],
+#                                  z_asl=[180, 142],
+#                                  pair_id='BTT_BCT',
+#                                  crs='epsg:32631')
 
 # path 12 - BCT -> IMU
 path_12 = sct.ScintillometerPair(x=[285440.6056, 284562.3107],
@@ -234,19 +340,19 @@ path_12 = sct.ScintillometerPair(x=[285440.6056, 284562.3107],
                                  pair_id='BCT_IMU',
                                  crs='epsg:32631')
 
-# path 13 - IMU -> BTT
-path_13 = sct.ScintillometerPair(x=[284562.3107, 282251.14],
-                                 y=[5712935.032, 5712486.47],
-                                 z_asl=[88, 180],
-                                 pair_id='IMU_BTT',
-                                 crs='epsg:32631')
+# # path 13 - IMU -> BTT
+# path_13 = sct.ScintillometerPair(x=[284562.3107, 282251.14],
+#                                  y=[5712935.032, 5712486.47],
+#                                  z_asl=[88, 180],
+#                                  pair_id='IMU_BTT',
+#                                  crs='epsg:32631')
 
-# path 14 - IMU -> SWT
-path_14 = sct.ScintillometerPair(x=[284562.3107, 285407],
-                                 y=[5712935.032, 5708599.83],
-                                 z_asl=[88, 44],
-                                 pair_id='IMU_SWT',
-                                 crs='epsg:32631')
+# # path 14 - IMU -> SWT
+# path_14 = sct.ScintillometerPair(x=[284562.3107, 285407],
+#                                  y=[5712935.032, 5708599.83],
+#                                  z_asl=[88, 44],
+#                                  pair_id='IMU_SWT',
+#                                  crs='epsg:32631')
 
 # path 15 - SCT -> SWT
 path_15 = sct.ScintillometerPair(x=[284450.1944, 285407],
@@ -258,26 +364,21 @@ path_15 = sct.ScintillometerPair(x=[284450.1944, 285407],
 bdsm_file = 'D:/Documents/scintools/example_inputs/rasters/height_surface_4m.tif'
 dem_file = 'D:/Documents/scintools/example_inputs/rasters/height_terrain_4m.tif'
 
-pt_11 = path_11.path_transect(bdsm_file, dem_file, hori_res)
-
-
+# pt_11 = path_11.path_transect(bdsm_file, dem_file, hori_res)
 pt_12 = path_12.path_transect(bdsm_file, dem_file, hori_res)
-pt_13 = path_13.path_transect(bdsm_file, dem_file, hori_res)
-pt_14 = path_14.path_transect(bdsm_file, dem_file, hori_res)
+# pt_13 = path_13.path_transect(bdsm_file, dem_file, hori_res)
+# pt_14 = path_14.path_transect(bdsm_file, dem_file, hori_res)
 pt_15 = path_15.path_transect(bdsm_file, dem_file, hori_res)
 
-plot_transects(path_11, pt_11,
-               path_12, pt_12,
-               path_13, pt_13,
-               path_14, pt_14,
-               path_15, pt_15,
-               hori_res)
-
 # plot_transects(path_11, pt_11,
-#                path_11, pt_11,
-#                path_11, pt_11,
-#                path_11, pt_11,
-#                path_11, pt_11,
+#                path_12, pt_12,
+#                path_13, pt_13,
+#                path_14, pt_14,
+#                path_15, pt_15,
 #                hori_res)
+
+plot_2_transects(path_12, pt_12,
+                 path_15, pt_15,
+                 hori_res)
 
 print('end')
